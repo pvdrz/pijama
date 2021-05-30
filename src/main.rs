08809a60f3b649b185f30036752e89fe13e4c7c4
Christian Poveda <git@pvdrz.com>
2021-05-30 11:07:07 -0500

    use proper variable names

--- src/main.rs
+++ src/main.rs
@@ -121,17 +121,17 @@ fn main() {
 struct DominatorTreeBuilder<'build> {
     fn_def: &'build FnDef,
     dfnum: IndexMap<Block, usize>,
-    ancestor: HashMap<Block, Block>,
-    idom: HashMap<Block, Block>,
-    samedom: HashMap<Block, Block>,
-    vertex: Vec<Block>,
-    parent: HashMap<Block, Block>,
+    ancestors: HashMap<Block, Block>,
+    idoms: HashMap<Block, Block>,
+    samedoms: HashMap<Block, Block>,
+    vertices: Vec<Block>,
+    parents: HashMap<Block, Block>,
     bucket: IndexMap<Block, HashSet<Block>>,
-    semi: HashMap<Block, Block>,
+    semidoms: HashMap<Block, Block>,
 }
 impl<'build> DominatorTreeBuilder<'build> {
     fn new(fn_def: &'build FnDef) -> Self {
-        let len_blocks = fn_def.preds.len();
+        let mut len_blocks = fn_def.preds.len();
 
         let mut dfnum = IndexMap::<Block, usize>::with_capacity(len_blocks);
         let mut bucket = IndexMap::<Block, HashSet<Block>>::with_capacity(len_blocks);
@@ -141,25 +141,27 @@ impl<'build> DominatorTreeBuilder<'build> {
             bucket.push(Default::default());
         }
 
+        len_blocks -= 1;
+
         Self {
             fn_def,
             dfnum,
-            ancestor: HashMap::with_capacity(len_blocks - 1),
-            idom: HashMap::with_capacity(len_blocks - 1),
-            samedom: HashMap::with_capacity(len_blocks - 1),
-            vertex: Vec::with_capacity(len_blocks),
-            parent: HashMap::with_capacity(len_blocks - 1),
+            ancestors: HashMap::with_capacity(len_blocks),
+            idoms: HashMap::with_capacity(len_blocks),
+            samedoms: HashMap::with_capacity(len_blocks),
+            vertices: Vec::with_capacity(len_blocks),
+            parents: HashMap::with_capacity(len_blocks),
             bucket,
-            semi: HashMap::with_capacity(len_blocks - 1),
+            semidoms: HashMap::with_capacity(len_blocks),
         }
     }
 
-    fn dfs(&mut self, pred: Option<Block>, block: Block) {
+    fn dfs(&mut self, parent: Option<Block>, block: Block) {
         if self.dfnum[block] == 0 {
-            self.dfnum[block] = self.vertex.len();
-            self.vertex.push(block);
-            if let Some(pred) = pred {
-                self.parent.insert(block, pred);
+            self.dfnum[block] = self.vertices.len();
+            self.vertices.push(block);
+            if let Some(pred) = parent {
+                self.parents.insert(block, pred);
             }
 
             for &succ in self.fn_def.succs[block].iter() {
@@ -168,59 +170,59 @@ impl<'build> DominatorTreeBuilder<'build> {
         }
     }
 
-    fn ancestor_with_lowest_semi(&self, mut v: Block) -> Block {
-        let mut u = v;
-        while let Some(&a) = self.ancestor.get(&v) {
-            if self.dfnum[self.semi[&v]] < self.dfnum[self.semi[&u]] {
-                u = v;
+    fn ancestor_with_lowest_semi(&self, mut block: Block) -> Block {
+        let mut result = block;
+        while let Some(&ancestor) = self.ancestors.get(&block) {
+            if self.dfnum[self.semidoms[&block]] < self.dfnum[self.semidoms[&result]] {
+                result = block;
             }
-            v = a;
+            block = ancestor;
         }
-        u
+        result
     }
 
     fn build(mut self) -> HashMap<Block, Block> {
         self.dfs(None, self.fn_def.entry);
 
-        for &n in self.vertex.iter().skip(1).rev() {
-            let p = self.parent[&n];
-            let mut s = p;
+        for &block in self.vertices.iter().skip(1).rev() {
+            let parent = self.parents[&block];
+            let mut semidom = parent;
 
-            for &v in self.fn_def.preds[n].iter() {
-                let s_prime = if self.dfnum[v] <= self.dfnum[n] {
-                    v
+            for &pred in self.fn_def.preds[block].iter() {
+                let semi_prime = if self.dfnum[pred] <= self.dfnum[block] {
+                    pred
                 } else {
-                    let a = self.ancestor_with_lowest_semi(v);
-                    self.semi[&a]
+                    let ancestor = self.ancestor_with_lowest_semi(pred);
+                    self.semidoms[&ancestor]
                 };
 
-                if self.dfnum[s_prime] < self.dfnum[s] {
-                    s = s_prime;
+                if self.dfnum[semi_prime] < self.dfnum[semidom] {
+                    semidom = semi_prime;
                 }
             }
 
-            self.semi.insert(n, s);
-            self.bucket[s].insert(n);
+            self.semidoms.insert(block, semidom);
+            self.bucket[semidom].insert(block);
 
-            self.ancestor.insert(n, p);
+            self.ancestors.insert(block, parent);
 
-            for &v in self.bucket[p].iter() {
+            for &v in self.bucket[parent].iter() {
                 let y = self.ancestor_with_lowest_semi(v);
-                if self.semi[&y] == self.semi[&v] {
-                    self.idom.insert(v, p);
+                if self.semidoms[&y] == self.semidoms[&v] {
+                    self.idoms.insert(v, parent);
                 } else {
-                    self.samedom.insert(v, y);
+                    self.samedoms.insert(v, y);
                 }
             }
-            self.bucket[p].clear();
+            self.bucket[parent].clear();
         }
 
-        for n in self.vertex.iter().skip(1) {
-            if let Some(m) = self.samedom.get(n) {
-                self.idom.insert(*n, self.idom[m]);
+        for block in self.vertices.iter().skip(1) {
+            if let Some(samedom) = self.samedoms.get(block) {
+                self.idoms.insert(*block, self.idoms[samedom]);
             }
         }
 
-        dbg!(self.idom)
+        dbg!(self.idoms)
     }
 }
