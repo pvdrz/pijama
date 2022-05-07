9bb7fbdf46383f672cc398798eb62b0d3a524701
Christian Poveda <git@pvdrz.com>
2022-05-07 16:14:00 -0500

    deduplicate code to add functions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{error::Error as StdError, fs::File, io::BufWriter};
 
 use object::{
-    write::{Object, SymbolSection},
+    write::{Object, SectionId, SymbolSection},
     Architecture, BinaryFormat, Endianness, SectionKind, SymbolFlags, SymbolKind, SymbolScope,
 };
 
@@ -14,44 +14,43 @@ fn main() -> Result<(), Box<dyn StdError>> {
     // Create the `.text` section.
     let section = obj.add_section(vec![], b".text".to_vec(), SectionKind::Text);
 
-    // The actual machine code we want to write
-    let code = &[
-        0x55, 0x48, 0x89, 0xe5, 0xb8, 0x0a, 0x00, 0x00, 0x00, 0x5d, 0xc3,
-    ];
+    add_function(
+        &mut obj,
+        section,
+        b"start",
+        &[
+            0x55, 0x48, 0x89, 0xe5, 0xb8, 0x0a, 0x00, 0x00, 0x00, 0x5d, 0xc3, 0x0f, 0x1f, 0x44,
+            0x00, 0x00,
+        ],
+    );
+
+    add_function(
+        &mut obj,
+        section,
+        b"duplicate",
+        &[
+            0x55, 0x48, 0x89, 0xe5, 0x89, 0x7d, 0xfc, 0x8b, 0x45, 0xfc, 0xc1, 0xe0, 0x01, 0x5d,
+            0xc3,
+        ],
+    );
+
+    // Write the object file.
+    obj.write_stream(file)?;
+
+    Ok(())
+}
+
+fn add_function(object: &mut Object, section: SectionId, name: &[u8], code: &[u8]) {
+    // The `GLOBAL` binding flag.
+    const BIND: u8 = 1;
+    // The `FUNC` type flag.
+    const TYPE: u8 = 2;
+    // The `DEFAULT` visibility flag.
+    const VIS: u8 = 0;
 
     // All this info is obtained from examining the `lib.o` file.
-    // The `GLOBAL` binding flag.
-    let bind = 1;
-    // The `FUNC` type flag.
-    let ty = 2;
-    // The `DEFAULT` visibility flag.
-    let vis = 0;
-
     let symbol = object::write::Symbol {
-        name: b"start".to_vec(),
-        value: 0,
-        size: code.len() as u64,
-        kind: SymbolKind::Text,
-        scope: SymbolScope::Linkage,
-        weak: false,
-        section: SymbolSection::Section(section),
-        flags: SymbolFlags::Elf {
-            st_info: (bind << 4) + (ty & 0xf),
-            st_other: vis & 0x3,
-        },
-    };
-
-    let symbol_id = obj.add_symbol(symbol);
-
-    obj.add_symbol_data(symbol_id, section, code, 16);
-    //
-    // The actual machine code we want to write
-    let code = &[
-        0x55, 0x48, 0x89, 0xe5, 0x89, 0x7d, 0xfc, 0x8b, 0x45, 0xfc, 0xc1, 0xe0, 0x01, 0x5d, 0xc3,
-    ];
-
-    let symbol = object::write::Symbol {
-        name: b"duplicate".to_vec(),
+        name: name.to_vec(),
         // It seems that `object` ignores this value so we can leave it be zero.
         value: 0,
         size: code.len() as u64,
@@ -60,17 +59,12 @@ fn main() -> Result<(), Box<dyn StdError>> {
         weak: false,
         section: SymbolSection::Section(section),
         flags: SymbolFlags::Elf {
-            st_info: (bind << 4) + (ty & 0xf),
-            st_other: vis & 0x3,
+            st_info: (BIND << 4) + (TYPE & 0xf),
+            st_other: VIS & 0x3,
         },
     };
 
-    let symbol_id = obj.add_symbol(symbol);
+    let symbol_id = object.add_symbol(symbol);
 
-    obj.add_symbol_data(symbol_id, section, code, 16);
-
-    // Write the object file.
-    obj.write_stream(file)?;
-
-    Ok(())
+    object.add_symbol_data(symbol_id, section, code, 16);
 }
