a217c2a7bdeaa4b4f763a6a268f4e400d45f9222
Christian Poveda <git@pvdrz.com>
2022-08-01 12:45:45 -0500

    fix add test

--- tests/asm/x86_64/add.asm
+++ tests/asm/x86_64/add.asm
@@ -1,37 +1,19 @@
 BITS 64
 
-%macro expand 1
-    add rax,%1
-    add rcx,%1
-    add rdx,%1
-    add rbx,%1
-    add rsp,%1
-    add rbp,%1
-    add rsi,%1
-    add rdi,%1
-    add r8,%1
-    add r9,%1
-    add r10,%1
-    add r11,%1
-    add r12,%1
-    add r13,%1
-    add r14,%1
-    add r15,%1
+%macro expand 16
+  %assign i 0
+  %rep %0
+    %rotate i
+    %define reg %1
+    %rotate -i
+
+    %rep %0
+      add %1,reg
+      %rotate 1
+    %endrep
+
+    %assign i i+1
+  %endrep
 %endmacro
 
-expand rax
-expand rcx
-expand rdx
-expand rbx
-expand rsp
-expand rbp
-expand rsi
-expand rdi
-expand r8
-expand r9
-expand r10
-expand r11
-expand r12
-expand r13
-expand r14
-expand r15
+expand rax,rcx,rdx,rbx,rsp,rbp,rsi,rdi,r8,r9,r10,r11,r12,r13,r14,r15
